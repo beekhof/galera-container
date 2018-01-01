1914d6fd059df8fe55a028a8c6839802dcf7484d
Andrew Beekhof <andrew@beekhof.net>
2018-01-02 10:13:21 +1100

    create a galera container

--- galera/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-count=1
-echo "$$ Container starting..."
-kubectl label --overwrite pods $HOSTNAME state=active
-echo "$$ Container done."
--- galera/stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "$$ Container stopping..."
-kubectl label --overwrite pods $HOSTNAME state=stopping
-echo "$$ Container stop done."
